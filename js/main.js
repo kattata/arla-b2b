f537da20750d9b3079a71dc7e120b6d261fcd7ed
oliverdotcom <55577972+oliverdotcom@users.noreply.github.com>
2020-11-08 11:18:59 +0100

    added chart js more coming

--- js/main.js
+++ js/main.js
@@ -137,3 +137,174 @@ function displayCalculation(result) {
 
 
 console.log(turnInputToNumbers());
+
+
+
+// ========== GLOBAL VARIABLES ========== //
+const _dataRef = _db.collection("sustainabilityData");
+let _sustainabilityData;
+
+// 1: data from firebase
+// listen for changes on _dataRef
+_dataRef.orderBy("year").onSnapshot(snapshotData => {
+    _sustainabilityData = []; // reset _sustainabilityData
+    snapshotData.forEach(doc => { // loop through snapshotData - like for of loop
+        let data = doc.data(); // save the data in a variable
+        data.id = doc.id; // add the id to the data variable
+        _sustainabilityData.push(data); // push the data object to the global array _sustainabilityData
+    });
+    console.log(_sustainabilityData);
+    appendSelfSuffiency(_sustainabilityData); // call appendCows with _sustainabilityData as function argument
+    appendCarbonFootprint(_sustainabilityData); //call appendCarbonFootprint with _sustainabilityData as function argument
+    appendMilkProduction(_sustainabilityData); //call appendMilkProduction with _sustainabilityData as function argument
+});
+
+// 2: preparing the data
+function prepareSuffiencyData(sustainabilityData) {
+    let self = [];
+    let years = [];
+    sustainabilityData.forEach(data => {
+        if (data.region === 'south') { // in this case we only want the data from 'north'
+            self.push(data.herdSelfSuffiencyInFeed);
+            years.push(data.year);
+        }
+    });
+    return {
+        self,
+        years
+    }
+}
+//3: appending the chart
+function appendSelfSuffiency(sustainabilityData) {
+    let data = prepareSuffiencyData(sustainabilityData);
+    console.log(data);
+    // generate chart
+    let chartContainer = document.querySelector('#myChart');
+    let chart = new Chart(chartContainer, {
+        type: 'line',
+        data: {
+            datasets: [{
+                data: data.self,
+                label: 'Number of Cows',
+                fill: true,
+                borderColor: "#e755ba",
+                backgroundColor: gradient,
+                pointBackgroundColor: "#55bae7",
+                pointBorderColor: "#55bae7",
+                pointHoverBackgroundColor: "#55bae7",
+                pointHoverBorderColor: "#55bae7",
+            }],
+            labels: data.years
+        }
+    });
+}
+
+// 2: preparing the data
+function prepareCarbonFootprintData(sustainabilityData) {
+    // prepare data
+    let carbonFootprint = [];
+    let years = [];
+    sustainabilityData.forEach(data => {
+        if (data.region === 'south') { // in this case we only want the data from 'north'
+            carbonFootprint.push(data.carbonFootprintWholeFarm);
+            years.push(data.year);
+        }
+    });
+
+    return {
+        carbonFootprint,
+        years
+    }
+}
+
+//3: appending the chart
+function appendCarbonFootprint(sustainabilityData) {
+    let data = prepareCarbonFootprintData(sustainabilityData);
+    console.log(data);
+
+    // generate chart
+    let chartContainer = document.querySelector('#carbonFootprint');
+    let chart = new Chart(chartContainer, {
+        type: 'line',
+        data: {
+            datasets: [{
+                data: data.carbonFootprint,
+                label: 'Carbon Footprint WholeFarm',
+                fill: false,
+                borderColor: "#e755ba",
+                backgroundColor: "#e755ba",
+                pointBackgroundColor: "#55bae7",
+                pointBorderColor: "#55bae7",
+                pointHoverBackgroundColor: "#55bae7",
+                pointHoverBorderColor: "#55bae7",
+            }],
+            labels: data.years
+        }
+    });
+}
+
+// 2: preparing the data
+function prepareMilkProductionData(sustainabilityData) {
+    let years = [];
+    let milkNorth = [];
+    let milkSouth = [];
+    sustainabilityData.forEach(data => {
+        if (data.region === 'north') { // condition testing whether the region is 'north' og 'south'
+            milkNorth.push(data.herdMilkProduction);
+            years.push(data.year);
+        } else if (data.region === 'south') {
+            milkSouth.push(data.herdMilkProduction);
+        }
+    });
+    return {
+        years,
+        milkNorth,
+        milkSouth
+    }
+}
+
+//3: appending the chart
+function appendMilkProduction(sustainabilityData) {
+    let data = prepareMilkProductionData(sustainabilityData);
+    console.log(data);
+
+    // generate chart
+    let chartContainer = document.querySelector('#milkProduction');
+    let chart = new Chart(chartContainer, {
+        type: 'line',
+        data: {
+            datasets: [
+                // first dataset - north
+                {
+                    data: data.milkNorth,
+                    label: 'Milk Production North',
+                    fill: false,
+                    borderColor: "#e755ba",
+                    backgroundColor: "#e755ba",
+                    pointBackgroundColor: "#55bae7",
+                    pointBorderColor: "#55bae7",
+                    pointHoverBackgroundColor: "#55bae7",
+                    pointHoverBorderColor: "#55bae7",
+                },
+                // second dataset - south
+                {
+                    label: 'Milk Production South',
+                    data: data.milkSouth,
+                    fill: false,
+                    borderColor: "#55bae7",
+                    backgroundColor: "#55bae7",
+                    pointBackgroundColor: "#e755ba",
+                    pointBorderColor: "#e755ba",
+                    pointHoverBackgroundColor: "#e755ba",
+                    pointHoverBorderColor: "#e755ba",
+                    type: 'line'
+                }
+            ],
+            labels: data.years
+        }
+    });
+}
+
+var gradient = chartContainer.createLinearGradient(0, 0, 0, 400);
+gradient.addColorStop(0, 'rgba(250,174,50,1)');
+gradient.addColorStop(1, 'rgba(250,174,50,0)');
