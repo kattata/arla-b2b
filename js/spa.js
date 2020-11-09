c912ab8b38379b582325e41a28b55e1417a202c6
Kasia Laniecka <kasia.laniecka@gmail.com>
2020-11-09 11:57:16 +0100

    logo leads to dashboard

--- js/spa.js
+++ js/spa.js
@@ -1,55 +1,9 @@
-// "use strict";
-
-// // hide all pages
-// function hideAllPages() {
-//     let pages = document.querySelectorAll(".page");
-//     for (let page of pages) {
-//         page.style.display = "none";
-//     }
-// }
-
-// // show page or tab
-// function showPage(pageId) {
-//     hideAllPages();
-//     document.querySelector(`#${pageId}`).style.display = "block";
-//     setActiveTab(pageId);
-// }
-
-// // sets active tabbar/ menu item
-// function setActiveTab(pageId) {
-//     let pages = document.querySelectorAll("nav>ul li");
-//     for (let page of pages) {
-//         if (`#${pageId}` === page.getAttribute("href")) {
-//             page.classList.add("active");
-
-//         } else {
-//             page.classList.remove("active");
-//         }
-//     }
-// }
-
-// // navigate to a new view/page by changing href
-// function navigateTo(pageId) {
-//     location.href = `#${pageId}`;
-// }
-
-// // set default page or given page by the hash url
-// // function is called 'onhashchange'
-// function pageChange() {
-//     let page = "home";
-//     if (location.hash) {
-//         page = location.hash.slice(1);
-//     }
-//     showPage(page);
-// }
-
-// pageChange(); // called by default when the app is loaded for the first time
 
 const navigation = document.querySelectorAll("nav ul li");
 let pages = document.querySelectorAll("#home .page");
 let survey = document.querySelector("#survey");
 let dashboard = document.querySelector(".dashboard");
-let login = document.querySelector('#login');
+let logo = document.querySelector('.arla-logo');
 
 // show login page first
 login.style.display = "flex";
@@ -104,4 +58,11 @@ function makeHomeActive() {
 }
 makeHomeActive();
 
+//logo leads to dashboard
+logo.addEventListener('click', () => {
+    hidePages()
+    hideTabs();
+    navigation[0].classList.add("active")
+    dashboard.style.display = 'grid';
+})
 
