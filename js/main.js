e37a107a5e5aea5f90430d68f981b0d48faf5377
oliverdotcom <55577972+oliverdotcom@users.noreply.github.com>
2020-11-09 10:23:26 +0100

    chart buttons (not styled yet)

--- js/main.js
+++ js/main.js
@@ -40,7 +40,6 @@ function appendDatatoPlaceholder(susData) {
         milkInput.placeholder = `${data.herdMilkProduction}`
     }
 }
-
 // ========== FIREBASE AUTH ========== //
 // Listen on authentication state change
 firebase.auth().onAuthStateChanged(function (user) {
@@ -170,7 +169,9 @@ function displayCalculation(result) {
 
 console.log(turnInputToNumbers());
 
-
+// make self suffiency and milk production charts initially display: none
+document.getElementById("myChart1").style.display = "none";
+document.getElementById("myChart2").style.display = "none";
 
 // ========== GLOBAL VARIABLES ========== //
 const _dataRef = _db.collection("sustainabilityData");
@@ -190,6 +191,50 @@ _dataRef.orderBy("year").onSnapshot(snapshotData => {
     appendCarbonFootprint(_sustainabilityData); //call appendCarbonFootprint with _sustainabilityData as function argument
     appendMilkProduction(_sustainabilityData); //call appendMilkProduction with _sustainabilityData as function argument
 });
+
+// CO2 PRODUCTION
+
+// 2: preparing the data
+function prepareCarbonData(sustainabilityData) {
+    let footprint = [];
+    let years = [];
+    sustainabilityData.forEach(data => {
+        if (data.region === 'south') { // in this case we only want the data from 'north'
+            footprint.push(data.carbonFootprintWholeFarm);
+            years.push(data.year);
+        }
+    });
+    return {
+        footprint,
+        years
+    }
+}
+//3: appending the chart
+function appendCarbonFootprint(sustainabilityData) {
+    let data = prepareCarbonData(sustainabilityData);
+    console.log(data);
+    // generate chart
+    let chartContainer = document.querySelector('#myChart');
+    let chart = new Chart(chartContainer, {
+        type: 'line',
+        data: {
+            datasets: [{
+                data: data.footprint,
+                label: 'CO2 Production',
+                fill: true,
+                borderColor: "#e755ba",
+                backgroundColor: gradient,
+                pointBackgroundColor: "#55bae7",
+                pointBorderColor: "#55bae7",
+                pointHoverBackgroundColor: "#55bae7",
+                pointHoverBorderColor: "#55bae7",
+            }],
+            labels: data.years
+        }
+    });
+}
+
+// SELF SUFFIENCY DATA
 
 // 2: preparing the data
 function prepareSuffiencyData(sustainabilityData) {
@@ -211,13 +256,13 @@ function appendSelfSuffiency(sustainabilityData) {
     let data = prepareSuffiencyData(sustainabilityData);
     console.log(data);
     // generate chart
-    let chartContainer = document.querySelector('#myChart');
+    let chartContainer = document.querySelector('#myChart1');
     let chart = new Chart(chartContainer, {
         type: 'line',
         data: {
             datasets: [{
                 data: data.self,
-                label: 'Number of Cows',
+                label: 'Self Suffiency in Feed',
                 fill: true,
                 borderColor: "#e755ba",
                 backgroundColor: gradient,
@@ -231,111 +276,49 @@ function appendSelfSuffiency(sustainabilityData) {
     });
 }
 
+// MILK PRODUCTION DATA
+
 // 2: preparing the data
-function prepareCarbonFootprintData(sustainabilityData) {
-    // prepare data
-    let carbonFootprint = [];
+function prepareMilkData(sustainabilityData) {
+    let mp = [];
     let years = [];
     sustainabilityData.forEach(data => {
         if (data.region === 'south') { // in this case we only want the data from 'north'
-            carbonFootprint.push(data.carbonFootprintWholeFarm);
+            mp.push(data.herdMilkProduction);
             years.push(data.year);
         }
     });
-
     return {
-        carbonFootprint,
+        mp,
         years
     }
 }
-
 //3: appending the chart
-function appendCarbonFootprint(sustainabilityData) {
-    let data = prepareCarbonFootprintData(sustainabilityData);
+function appendMilkProduction(sustainabilityData) {
+    let data = prepareMilkData(sustainabilityData);
     console.log(data);
-
     // generate chart
-    let chartContainer = document.querySelector('#carbonFootprint');
+    let chartContainer = document.querySelector('#myChart2');
     let chart = new Chart(chartContainer, {
         type: 'line',
         data: {
             datasets: [{
-                data: data.carbonFootprint,
-                label: 'Carbon Footprint WholeFarm',
-                fill: false,
+                data: data.mp,
+                label: "Milk Production",
+                fill: true,
                 borderColor: "#e755ba",
-                backgroundColor: "#e755ba",
+                backgroundColor: gradient,
                 pointBackgroundColor: "#55bae7",
                 pointBorderColor: "#55bae7",
                 pointHoverBackgroundColor: "#55bae7",
                 pointHoverBorderColor: "#55bae7",
             }],
-            labels: data.years
+            labels: data.years,
         }
     });
 }
 
-// 2: preparing the data
-function prepareMilkProductionData(sustainabilityData) {
-    let years = [];
-    let milkNorth = [];
-    let milkSouth = [];
-    sustainabilityData.forEach(data => {
-        if (data.region === 'north') { // condition testing whether the region is 'north' og 'south'
-            milkNorth.push(data.herdMilkProduction);
-            years.push(data.year);
-        } else if (data.region === 'south') {
-            milkSouth.push(data.herdMilkProduction);
-        }
-    });
-    return {
-        years,
-        milkNorth,
-        milkSouth
-    }
-}
 
-//3: appending the chart
-function appendMilkProduction(sustainabilityData) {
-    let data = prepareMilkProductionData(sustainabilityData);
-    console.log(data);
-
-    // generate chart
-    let chartContainer = document.querySelector('#milkProduction');
-    let chart = new Chart(chartContainer, {
-        type: 'line',
-        data: {
-            datasets: [
-                // first dataset - north
-                {
-                    data: data.milkNorth,
-                    label: 'Milk Production North',
-                    fill: false,
-                    borderColor: "#e755ba",
-                    backgroundColor: "#e755ba",
-                    pointBackgroundColor: "#55bae7",
-                    pointBorderColor: "#55bae7",
-                    pointHoverBackgroundColor: "#55bae7",
-                    pointHoverBorderColor: "#55bae7",
-                },
-                // second dataset - south
-                {
-                    label: 'Milk Production South',
-                    data: data.milkSouth,
-                    fill: false,
-                    borderColor: "#55bae7",
-                    backgroundColor: "#55bae7",
-                    pointBackgroundColor: "#e755ba",
-                    pointBorderColor: "#e755ba",
-                    pointHoverBackgroundColor: "#e755ba",
-                    pointHoverBorderColor: "#e755ba",
-                    type: 'line'
-                }
-            ],
-            labels: data.years
-        }
-    });
-}
 
 var gradient = chartContainer.createLinearGradient(0, 0, 0, 400);
 gradient.addColorStop(0, 'rgba(250,174,50,1)');
@@ -480,3 +463,27 @@ console.log('Days left: ' + surveyDeadline);
 const surveyDashboardDays = document.querySelector(".text-wrapper-survey p:nth-of-type(2)");
 
 surveyDashboardDays.innerHTML = `${surveyDeadline} DAYS LEFT`
+
+function hideFunction() {
+    if (myChart.style.display === "none") {
+        myChart.style.display = "block";
+        myChart1.style.display = "none";
+        myChart2.style.display = "none";
+    }
+}
+
+function hideFunction1() {
+    if (myChart1.style.display === "none") {
+        myChart1.style.display = "block";
+        myChart.style.display = "none";
+        myChart2.style.display = "none";
+    }
+}
+
+function hideFunction2() {
+    if (myChart2.style.display === "none") {
+        myChart2.style.display = "block";
+        myChart.style.display = "none";
+        myChart1.style.display = "none";
+    }
+}
